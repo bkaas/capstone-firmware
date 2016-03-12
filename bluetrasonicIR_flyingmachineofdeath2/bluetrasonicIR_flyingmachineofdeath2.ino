f649e65dab90fcec25f22744e916bef6658d3453
Alex Ianovski <Alex Ianovski>
2016-03-11 21:52:05 -0500

    With PID Library

--- bluetrasonicIR_flyingmachineofdeath2/bluetrasonicIR_flyingmachineofdeath2.ino
+++ bluetrasonicIR_flyingmachineofdeath2/bluetrasonicIR_flyingmachineofdeath2.ino
@@ -1,3 +1,5 @@
+
+#include <PID_v1.h>
 #include <SoftwareSerial.h>
 
 #include <avr/power.h>
@@ -6,18 +8,30 @@
 #define F_CPU 16000000
 
 #define MIN(a, b) ((a < b) ? a : b)
-#define MAX(a, b) ((a > b) ? a : b) 
+#define MAX(a, b) ((a > b) ? a : b)
 
 #define timer_ticks 420 // compare value adjusted for CTC mode, 1/38000 sec = 420
 #define LEDPIN 13 //pulsing LED
 #define txrxPin A0
-#define srfAddress2 0x03
+#define srfAddress2 0x01
 #define getRange 0x54                                        // Byte used to get range from SRF01 in cm 
+
+//PID:
+//Define Variables we'll be connecting to
+double Input, Output;
+double dist = 0;
+double setPoint;
+double p = 3, i=5, d=1;
+//Specify the links and initial tuning parameters
+PID myPID(&dist, &Output, &setPoint, p, i, d, DIRECT);
+//
+
+
 
 bool ultra = 0;
 
 char blueval;
-int dist = 0;
+
 float thrLevel = 1201;
 int rlLevel = 1500;
 int ptchLevel = 1500;
@@ -28,7 +42,7 @@ String tmpStr;
 int trimStep = 2;
 float Kp = 300.0;
 int tmpInt;
-int setPoint = 40;
+
 int ultraMin = 1750;
 
 // IR sensors
@@ -44,16 +58,19 @@ byte irPin4 = 13;
 
 
 SoftwareSerial UltrasonicBus(txrxPin, txrxPin);
-SoftwareSerial blue(8,7); //RX, TX
+SoftwareSerial blue(8, 7); //RX, TX
 
 void setup() {
   
-  Serial.begin(115200);
-  while(!Serial && !blue.available());
-  UltrasonicBus.begin(9600);
-  while(!Serial && !UltrasonicBus.available());
-  blue.begin(9600);
+  setPoint = 40;
+  myPID.SetMode(AUTOMATIC);
   
+  Serial.begin(115200);
+  while (!Serial && !blue.available());
+  UltrasonicBus.begin(9600);
+  while (!Serial && !UltrasonicBus.available());
+  blue.begin(9600);
+
   // set sensor pins to detect objects
   pinMode(irPin1, INPUT);
   pinMode(irPin2, INPUT);
@@ -62,20 +79,21 @@ void setup() {
 
   // set output pin for pulsing LED
   pinMode(LEDPIN, OUTPUT);
-  
+
 }
 
 void loop()  {
 
-if( ultra ) {
-  UltrasonicBus.listen();
-  if (UltrasonicBus.isListening()) {
-    dist = doRange(srfAddress2);
-    thrLevel = thrPID(dist);
-    tmpInt = int(thrLevel);
-    Serial.print("t" + String(tmpInt));
+  if ( ultra ) {
+    UltrasonicBus.listen();
+    if (UltrasonicBus.isListening()) {
+      dist = doRange(srfAddress2);
+      thrLevel = thrPID(dist);
+
+      tmpInt = int(thrLevel);
+      Serial.print("t" + String(tmpInt));
+    }
   }
-}
 
   blue.listen();
   if (blue.isListening()) {
@@ -83,20 +101,20 @@ if( ultra ) {
     blueval = blue.read();
   }
 
-  switch(blueval) {
-    
+  switch (blueval) {
+
     case 'a': //if(blueval == 'a'){  //ARM
       Serial.print(blueval);
       delay(20); //if we delay above, do we need delay here?
       thrLevel = 1201;
       break;
-      
+
     case 'u': //if(blueval == 'u'){  //toggle ultrasonic measurements
       ultra ^= 1;
       break;
-      
+
     case 't': //if (blueval == 't') {
-      for(int i = 0; i < 4; i++){
+      for (int i = 0; i < 4; i++) {
         c = blue.read();
         thr += (char)c;
       }
@@ -104,49 +122,49 @@ if( ultra ) {
       thrLevel = thr.toInt();
       thr = "";
       break;
-      
+
     case 'w': //if(blueval == 'w'){   //down roll midVal (west)
       rlLevel -= trimStep;
       rlLevel = MAX(rlLevel, 1370);  //1370 trim min from multiwii
       Serial.print("r" + String(rlLevel));
       delay(20);
       break;
-      
+
     case 'e': //if(blueval == 'e'){  //up roll midVal (east)
       rlLevel += trimStep;
       rlLevel = MIN(rlLevel, 1585);  //1585 trim max from multiwii
       Serial.print("r" + String(rlLevel));
       delay(20);
       break;
-      
+
     case 's': //if(blueval == 's'){  //down pitch midVal (south)
       ptchLevel -= trimStep;
       ptchLevel = MAX(ptchLevel, 1370);
       Serial.print("p" + String(ptchLevel));
       delay(20);
       break;
-      
+
     case 'n': //if(blueval == 'n'){  //up pitch midVal (north)
       ptchLevel += trimStep;
       ptchLevel = MIN(ptchLevel, 1585);
       Serial.print("p" + String(ptchLevel));
       delay(20);
       break;
-      
+
     case 'l': //if(blueval == 'l'){  //down yaw midVal (left)
       yawLevel -= trimStep;
       yawLevel = MAX(yawLevel, 1370);
       Serial.print("y" + String(yawLevel));
       delay(20);
       break;
-      
+
     case 'r': //if(blueval == 'r'){  //up yaw midVal (right)
       yawLevel += trimStep;
       yawLevel = MIN(yawLevel, 1585);
       Serial.print("y" + String(yawLevel));
       delay(20);
       break;
-      
+
     case 'z': //if(blueval == 'z'){  //reset roll and pitch
       ptchLevel = 1500;
       rlLevel = 1500;
@@ -158,18 +176,35 @@ if( ultra ) {
       Serial.print("y" + String(yawLevel));
       delay(20);
       break;
-      
-    case 'k': //if (blueval == 'k') {
-      for(int i = 0; i < 4; i++){
+
+    case 'p': //if (blueval == 'k') {
+      for (int i = 0; i < 4; i++) {
         c = blue.read();
         tmpStr += (char)c;
       }
-      Kp = tmpStr.toInt();
+      p = double(tmpStr.toInt());
       tmpStr = "";
       break;
 
+    case 'i': //if (blueval == 'k') {
+      for (int i = 0; i < 4; i++) {
+        c = blue.read();
+        tmpStr += (char)c;
+      }
+      i = double(tmpStr.toInt());
+      tmpStr = "";
+      break;
+      
+      case 'd': //if (blueval == 'k') {
+      for (int i = 0; i < 4; i++) {
+        c = blue.read();
+        tmpStr += (char)c;
+      }
+      d = double(tmpStr.toInt());
+      tmpStr = "";
+      break;
     case 'h': //height, changes setpoint
-      for(int i = 0; i < 3; i++){
+      for (int i = 0; i < 3; i++) {
         c = blue.read();
         tmpStr += (char)c;
       }
@@ -177,51 +212,49 @@ if( ultra ) {
       tmpStr = "";
       break;
 
-    case 'm': //minimum ultra val
-      for(int i = 0; i < 4; i++){
-        c = blue.read();
-        tmpStr += (char)c;
-      }
-      ultraMin = tmpStr.toInt();
-      tmpStr = "";
-      break;
-    }
+    
+  }
   //    if(blueval == '!'){  //tell us what the roll and pitch is
   //      blue.write(rlLevel);
   //      delay(20);
   //      blue.write(ptchLevel);
   //      delay(20);
   //    }
-    
+
 
   /******ROLL*******/
-//  if (digitalRead(digitalPin)==LOW){ //IR sensor sees something
-//    Serial.print("rg");  //roll go
-//    sCount = 0;
-//    gCount++;
-//    tmp = gCount;
-//  }
-//  
-//  else if(digitalRead(digitalPin)==HIGH) {
-//    if(sCount < tmp) { 
-//      Serial.print("rc");
-//    }
-//    else {
-//      Serial.print("rs"); //roll stop 
-//    }
-//    sCount++;
-//    gCount = 0;
-//  }
-    
+  //  if (digitalRead(digitalPin)==LOW){ //IR sensor sees something
+  //    Serial.print("rg");  //roll go
+  //    sCount = 0;
+  //    gCount++;
+  //    tmp = gCount;
+  //  }
+  //
+  //  else if(digitalRead(digitalPin)==HIGH) {
+  //    if(sCount < tmp) {
+  //      Serial.print("rc");
+  //    }
+  //    else {
+  //      Serial.print("rs"); //roll stop
+  //    }
+  //    sCount++;
+  //    gCount = 0;
+  //  }
+
 }
 
 float thrPID(int ultraDist) {
   float thrErr = 0;
-
-  thrErr = (setPoint - ultraDist);
-  thrLevel = thrLevel + thrErr*Kp/100.0;
-  thrLevel = MIN(thrLevel, 1999);
-  thrLevel = MAX(thrLevel, ultraMin);
+  //myPID.SetTunings(3, 5, 2);
+  myPID.Compute();
+  thrLevel = 1800+0.784*Output;
+  
+  
+  //thrErr = (setPoint - ultraDist);
+  //thrLevel = thrLevel + thrErr * Kp / 100.0;
+  
+  //thrLevel = MIN(thrLevel, 1999);
+  //thrLevel = MAX(thrLevel, ultraMin);
 
   return thrLevel;
 }
