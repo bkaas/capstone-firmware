0d0f93172c8fa4866587edc945511a273b319e90
Brendan Kaas <bren.kaas@gmail.com>
2016-03-31 19:10:40 -0400

    back to before the cleaned code

--- cleaned-code/cleaned-code.ino
+++ cleaned-code/cleaned-code.ino
@@ -18,6 +18,7 @@
 double Input, Output;
 double dist = 0;
 double setPoint;
+
 //Below setpoint constants:
 double p = 2.8, i = 0.0, d = 0.0; //p=2.7, i=0.81
 
@@ -29,21 +30,25 @@ double pa = 0.8*p, ia = 0.8*i, da = 0.0;
 PID myPID(&dist, &Output, &setPoint, p, i, d, DIRECT);
 //
 
-
+//Variables of the major components
 char blueval;
 bool ultra = 0;
 bool infrared = 0;
+bool goEvade = 0;
 
+//
 float thrLevel = 1201;
 int midVal[3] = {1500, 1500, 1500}; // pitch roll yaw midVal[1] = 1500; int midVal[0] = 1500; int midVal[2] = 1500;
-int minThrottle = 1760;
+int minThrottle;
 int trimStep = 2;
 int ptchLevel; int rlLevel;
 
+//Temporary values for data handling
 uint8_t c;
 String thr;
 String tmpStr;
 int tmpInt;
+int tmpPitch, tmpRoll, tmpThrottle;
 
 // IR sensors
 byte irPinN = 12;
@@ -53,108 +58,134 @@ byte irPinW = A5;
 byte enablePin = 13;
 
 // Roll and Pitch movement stuff
-int adjustment = 100;
+int adjustment;
 bool state[4];  //north, east, south, west in that order
-int count[4] = {0, 0, 0, 0};
-int comp[4] = {0, 0, 0, 0};
-int countPrev[4];
-int tmpDir[4];
-String dir[2] = {"p","r"};
 int timeout=0, timeout1=0;
 int direc, direc1;
+unsigned long currentMillis;
 unsigned long previousMillis1, previousMillis2, previousMillis3, previousMillis4;
 bool go = 1, go1 = 1;
-int dip = 1000;
+int dip;
+int minDist;
+float compFrac;
 
 SoftwareSerial UltrasonicBus(txrxPin, txrxPin);
 SoftwareSerial blue(8, 7); //RX, TX
 
 void setup() {
+  myPID.SetMode(AUTOMATIC); // Initialize the PID
 
-  setPoint = 100;
-  myPID.SetMode(AUTOMATIC);
-
-  Serial.begin(115200);
-  while (!Serial && !blue.available());
-  UltrasonicBus.begin(9600);
-  while (!Serial && !UltrasonicBus.available());
-  blue.begin(9600);
-
-  // set sensor pins to detect objects
-  pinMode(irPinN, INPUT);
-  pinMode(irPinE, INPUT);
-  pinMode(irPinS, INPUT);
-  pinMode(irPinW, INPUT);
-
-  pinMode(enablePin, OUTPUT);
-  
-
+  loadSerials(); // Initialize serial communication
+  loadDefaults(); // Load defaults
+  initializePins(); // Initialize pinModes
 }
 
 void loop()  {
-  unsigned long currentMillis = millis();
+  currentMillis = millis();
   
-  if ( ultra ) {
+  if ( ultra ) Serial.print("t" + String(runUltra()));
+  if ( infrared && dist > minDist) {
+    tmpPitch = runPitchIR();
+    tmpRoll = runRollIR();
+    if (tmpPitch > 0) Serial.print("p" + String(tmpPitch));
+    if (tmpRoll > 0) Serial.print("r" + String(tmpRoll));
+  }
+
+  tmpPitch = runPitchIR();
+  if (tmpPitch > 0) Serial.print("p" + String(tmpPitch));
+  state[0] = 0;
+  runBlue();
+}
+
+/**************************************
+ * All called functions are below. Five catagories:
+ * 1) Setting pinModes and defaults for adjustable parameters. 
+ * 2) Communcation with Ultrasonic sensor.
+ * 3) Send to PID black box.
+ * 4) Main loop blocks (ultrasonic, IR, Bluetooth).
+ */
+
+
+// 1. Defaults for adjustable parameters.
+void loadDefaults() {
+  setPoint = 100; // Setpoint height in cm, adjust with 'h'
+  minThrottle = 1740; // Minimum throttle on quad firmware, adjust with 'm'
+  adjustment = 100; // Evasion strength, in pitch/roll increment, adjust with 'j'
+  dip = 1000; // Time in millis before compensating, adjust with 'g'
+  minDist = 40; // Minimum elevation to engage IR sensors, adjust with 'x'
+  compFrac = 0.75;  //fraction of dip to compensate 'g'
+}
+
+
+// 2. Ultrasonic main loop block.
+int runUltra() {
     UltrasonicBus.listen();
     if (UltrasonicBus.isListening()) {
 
       dist = doRange(srfAddress2);
       thrLevel = thrPID();
       tmpInt = int(thrLevel);
-      Serial.print("t" + String(tmpInt));
     }
-  }
+    return tmpInt;
+}
+
+// 2. IR Pitch main loop block.
+int runPitchIR() {
+  /****PITCH****/
+  //state[0] = !digitalRead(irPinN); //fucked wit dis
+  state[2] = 0; //!digitalRead(irPinS);
   
-  if ( infrared && dist > 5 ) {
-    
-    /****PITCH****/
-    state[0] = digitalRead(irPinN);
-    state[2] = digitalRead(irPinS);
-    
-    if((!state[0]-!state[2]) && go){
-      Serial.print("p" + String(midVal[0] + adjustment*(!state[2] - !state[0])));
+    if((state[0]-state[2]) && go){
       previousMillis1 = currentMillis;
-      direc = adjustment*(!state[2] - !state[0]);
+      direc = adjustment*(state[2] - state[0]);
       timeout = 1;
       go = 0;
+      return (midVal[0] + adjustment*(state[2] - state[0]));
     }
-    if (((currentMillis-previousMillis1) >= dip) && timeout==1 && state[0] && state[2]){
-       Serial.print("p" + String(midVal[0] - (int)(0.75*direc)));
+    else if (((currentMillis-previousMillis1) >= dip) && timeout==1 && !state[0] && !state[2]){
        previousMillis2 = currentMillis;
        timeout =2;
-       direc = 0;
        go = 1;
+       return (midVal[0] - (int)(compFrac*direc));
      }
-    if (((currentMillis-previousMillis2) >= 500) && timeout==2){
-       Serial.print("p" + String(midVal[0]));
-       timeout =0;
+     else if (((currentMillis-previousMillis2) >= 500) && timeout==2){
+       timeout = 0;
+       return midVal[0];
      }
+     else {
+       goEvade = 0;
+       return 0;
+     }
+}
 
-
-    /****ROLL****/
-    state[1] = digitalRead(irPinE);
-    state[3] = digitalRead(irPinW);
-    
-    if((!state[1]-!state[3]) && go1){
-      Serial.print("r" + String(midVal[1] + adjustment*(!state[3] - !state[1])));
+// 2. IR Roll main loop block.
+int runRollIR() {
+    /****ROLL****/ 
+  state[1] = 0;//!digitalRead(irPinE);
+  state[3] = 0;//!digitalRead(irPinW); 
+        
+    if((state[1]-state[3]) && go1){
       previousMillis3 = currentMillis;
-      direc1 = adjustment*(!state[3] - !state[1]);
+      direc1 = adjustment*(state[3] - state[1]);
       timeout1 = 1;
       go1 = 0;
+      return (midVal[1] + adjustment*(state[3] - state[1]));
     }
-    if (((currentMillis-previousMillis3) >= dip) && timeout1==1 && state[1] && state[3]){
-       Serial.print("r" + String(midVal[1] - (int)(0.75*direc1)));
+    else if (((currentMillis-previousMillis3) >= dip) && timeout1==1 && !state[1] && !state[3]){
        previousMillis4 = currentMillis;
        timeout1 =2;
-       direc1 = 0;
        go1 = 1;
+       return (midVal[1] - (int)(compFrac*direc1));
      }
-    if (((currentMillis-previousMillis4) >= 500) && timeout1==2){
-       Serial.print("r" + String(midVal[1]));
-       timeout1 =0;
+     else if (((currentMillis-previousMillis4) >= 500) && timeout1==2){
+       timeout1 = 0;
+       return midVal[1];
      }
-  }
+     else return 0;
+}
 
+// 2. Bluetooth main loop block.
+void runBlue() {
   blue.listen();
   if (blue.isListening()) {
     delay(50); //does this really need to be this long? 50 sort of works?
@@ -165,14 +196,11 @@ void loop()  {
 
     case 'a': //if(blueval == 'a'){  //ARM
       Serial.print(blueval);
-      //delay(20); //if we delay above, do we need delay here?
-      thrLevel = 1201;
       break;
 
     case 'u': //if(blueval == 'u'){  //toggle ultrasonic measurements, with ramp up to minimize initial error by Dan
-      Serial.print('t'+ String(minThrottle+50));
+      Serial.print("t" + String(minThrottle + 50));
       delay(30);
-      
       ultra ^= 1;
       break;
 
@@ -187,7 +215,6 @@ void loop()  {
         thr += (char)c;
       }
       Serial.print("t" + thr);
-      thrLevel = thr.toInt();
       thr = "";
       break;
 
@@ -233,16 +260,13 @@ void loop()  {
       delay(20);
       break;
 
-    case 'z': //if(blueval == 'z'){  //reset roll and pitch
-      midVal[0] = 1500;
-      midVal[1] = 1500;
-      midVal[2] = 1500;
-      Serial.print("p" + String(midVal[0]));
-      delay(20);
-      Serial.print("r" + String(midVal[1]));
-      delay(20);
-      Serial.print("y" + String(midVal[2]));
-      delay(20);
+    case 'x':   //set minimum distance to engage IRs
+      for (int i = 0; i < 4; i++) {
+        c = blue.read();
+        tmpStr += (char)c;
+      }
+      minDist = tmpStr.toInt();
+      tmpStr = "";
       break;
 
     case 'p': //if (blueval == 'k') {
@@ -293,7 +317,7 @@ void loop()  {
       tmpStr = "";
       break;
 
-    case 'g': //
+    case 'o': //
       for (int i = 0; i < 4; i++) {
         c = blue.read();
         tmpStr += (char)c;
@@ -317,9 +341,24 @@ void loop()  {
       Serial.print(blueval);
       delay(20); //if we delay above, do we need delay here?
       break;
+
+    case '!': //IR evade now (test)
+      state[0] = 1;
+      break;
+
+    case 'g': //fraction of dip to compensate
+      for (int i = 0; i < 4; i++) {
+        c = blue.read();
+        tmpStr += (char)c;
+      }
+      compFrac = double(tmpStr.toInt());
+      compFrac = compFrac / 1000.0;
+      tmpStr = "";
+      break;
   }
 }
 
+// 3. PID black box.
 float thrPID() {
   float thrErr = 0;
   myPID.Compute();
@@ -328,6 +367,26 @@ float thrPID() {
   return thrLevel;
 }
 
+// 4. Setup for serial/software serials.
+void loadSerials() {
+  Serial.begin(115200);
+  while (!Serial && !blue.available());
+  UltrasonicBus.begin(9600);
+  while (!Serial && !UltrasonicBus.available());
+  blue.begin(9600);
+}
+
+// 4. pinMode setup for IR sensors.
+void initializePins() {
+  // set sensor pins to detect objects
+  pinMode(irPinN, INPUT);
+  pinMode(irPinE, INPUT);
+  pinMode(irPinS, INPUT);
+  pinMode(irPinW, INPUT);
+  pinMode(enablePin, OUTPUT);
+}
+
+// 4. Communication with Ultrasonic sensor.
 int doRange(byte Address) {
   SRF01_Cmd(Address, getRange);                              // Calls a function to get range from SRF01
   while (UltrasonicBus.available() < 2);                     // Waits to get good data
